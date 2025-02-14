34017fbe0a455e191044ff7fcc550e44a049fd6f
efletcherPIFSC <37851243+efletcherPIFSC@users.noreply.github.com>
2025-02-14 11:00:36 -1000

    revert ss_growthseries

--- R/ss_output_export_agepro.R
+++ R/ss_output_export_agepro.R
@@ -170,17 +170,14 @@ export_ss_objectlist_year <- function (ss_objectlist, ss_agepro){
   Mat_Slope <- get_ss_objectlist_parameter(ss_objectlist, "Mat_slope_Fem_GP_1")
   Mat_50 <- get_ss_objectlist_parameter(ss_objectlist, "Mat50%_Fem_GP_1")
 
-  #Macro variable to shorten ss_objectlist$growthseries
-  ss_growthseries <- ss_objectlist$growthseries
-
   which_Latage <-
     which(
-      ss_growthseries$Yr == yr_end &
-        ss_growthseries$Seas == 1 &
-        ss_growthseries$SubSeas == 1
+      ss_objectlist$growthseries$Yr == yr_end &
+        ss_objectlist$growthseries$Seas == 1 &
+        ss_objectlist$growthseries$SubSeas == 1
     )
 
-  LatAge <- ss_growthseries[which_Latage, 6:ncol(ss_growthseries)]
+  LatAge <- ss_objectlist$growthseries[which_Latage, 6:ncol(ss_objectlist$growthseries)]
 
   ss_agepro[["MatAtAge"]] <- 1 / (1 + exp(Mat_Slope*(LatAge-Mat_50)))
   ss_agepro[["MatAtAgeCV"]] <- rep(0.01, ss_agepro$MaxAge)
